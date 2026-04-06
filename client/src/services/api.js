3f8d722c0f56ec215d481172090991dad9320e4d
chris <kaijames437@gmail.com>
2026-04-06 13:46:43 -0400

    Debugged some methods that required inputs within the link

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,41 @@
+//Method to create a user
 export const createUser = async (data) => {
-    const res = await fetch("http://localhost:4000/users", {
+    const res = await fetch("http://localhost:4000/create/user", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(data)
     });
     return res.json()
-};
+}
+
+//Method to delete user by id
+export const deleteUser = async (data) => {
+    const res = await fetch("http://localhost:4000/delete/user/" + data, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+    });
+    return res.json()
+}
+
+//Method to update a users username
+export const updateUsername = async (data) => {
+    const res = await fetch("http://localhost:4000/update/user/username/+" + data.username, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
+    return res.json()
+}
+
+//Method to update a users email
+export const updateEmail = async (data) => {
+    const res = await fetch("http://localhost:4000/update/user/email/" + data.email, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
+    return res.json()
+}
 
 //Method to grab all users
 export function getAllUsers(){
@@ -13,6 +43,36 @@ export function getAllUsers(){
 }
 
 //Method to grab a user by id
-export function getUserById(id){
-    return fetch(`/get/user/${id}`).then(res => res.json());
+export function getUserById(userId){
+    return fetch(`/get/user/id/${userId}`).then(res => res.json());
+}
+
+//Method to grab a user by username
+export function getUserByUsername(username){
+    return fetch(`/get/user/username/${username}`).then(res => res.json());
+}
+
+//Method to grab a user by email
+export function getUserByEmail(email){
+    return fetch(`/get/user/email/${email}`).then(res => res.json());
+}
+
+//Method to grab a users message by the message id
+export function getUserMessagesById(userId, messageId){
+    return fetch(`/get/user/${userId}/message/${messageId}`).then(res => res.json());
+}
+
+//Method to grab all of a users messages 
+export function getAllUserMessages(userId){
+    return fetch(`/get/user/${userId}/messages/all`).then(res => res.json());
+}
+
+//Method to create a message from a user
+export const createMessage = async (userId, content) => {
+    const res = await fetch("http://localhost:4000/create/user/" + userId + "/message", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ content })
+    });
+    return res.json();
 }
